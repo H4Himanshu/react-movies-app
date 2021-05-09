00ac40875e34735c0c534c87870d1dd3b9a6bab9
himanshu chawla <himanshuchawla4393@gmail.com>
2021-05-09 11:57:45 +0530

    Added modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,32 +2,39 @@ import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg'
+import Modal from 'react-modal';
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            modelIsOpen: false
+        }
+    }
+
+    openModalHandler = () => {
+        this.setState({ modelIsOpen: true })
+    }
+
+    closeModalHandler = () => {
+        this.setState({ modelIsOpen: false })
+    }
+
     render() {
         return (
-            // <div className="header-comp">
-            //     <header className="app-header">
-            //         <div class="container">
-            //             <img src={logo} className="app-logo" alt="logo"></img>
-            //             <Button variant="contained" color="default">
-            //                                 Login
-            //                             </Button>
-            //         </div>
-            //     </header>
-            // </div>
-            <nav>
+            <div>
                 <header className="app-header">
                     <img src={logo} className="app-logo" alt="logo"></img>
                     <ul>
                         <li>
-                            <Button variant="contained" color="default">
-                                Login
-                        </Button>
+                            <Button variant="contained" color="default" onClick={this.openModalHandler}> Login</Button>
                         </li>
                     </ul>
                 </header>
-            </nav>
+                <Modal ariaHideApp={false} isOpen={this.state.modelIsOpen} content="Login" onRequestClose={this.closeModalHandler}>
+                </Modal>
+            </div>
+
         )
     }
 }
