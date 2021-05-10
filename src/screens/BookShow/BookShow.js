84ad1f8c5d13b686a3e06052db1628be6a7c6a51
himanshu chawla <himanshuchawla4393@gmail.com>
2021-05-10 23:40:04 +0530

    Added unit price, total price and book show button on book show page

--- src/screens/BookShow/BookShow.js
+++ src/screens/BookShow/BookShow.js
@@ -15,12 +15,25 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 class BookShow extends Component {
     constructor() {
         super();
         this.state = {
-            location: ""
+            location: "",
+            language: "",
+            showDate: "",
+            showTime: "",
+            tickets: 0,
+            unitPrice: 500,
+            availableTickets: 20,
+            reqLocation: "dispNone",
+            reqLanguage: "dispNone",
+            reqShowDate: "dispNone",
+            reqShowTime: "dispNone",
+            reqTickets: "dispNone"
         }
     }
 
@@ -30,6 +43,30 @@ class BookShow extends Component {
 
     locationChangeHandler = event => {
         this.setState({ location: event.target.value });
+    }
+
+    languageChangeHandler = event => {
+        this.setState({ language: event.target.value });
+    }
+
+    showDateChangeHandler = event => {
+        this.setState({ showDate: event.target.value });
+    }
+
+    showTimeChangeHandler = event => {
+        this.setState({ showTime: event.target.value });
+    }
+
+    ticketsChangeHandler = (event) => {
+        this.setState({ tickets: event.target.value })
+    }
+
+    bookShowButtonHandler = () => {
+        this.state.location === "" ? this.setState({ reqLocation: "dispBlock" }) : this.setState({ reqLocation: "dispNone" });
+        this.state.language === "" ? this.setState({ reqLanguage: "dispBlock" }) : this.setState({ reqLanguage: "dispNone" });
+        this.state.showDate === "" ? this.setState({ reqShowDate: "dispBlock" }) : this.setState({ reqShowDate: "dispNone" });
+        this.state.showTime === "" ? this.setState({ reqShowTime: "dispBlock" }) : this.setState({ reqShowTime: "dispNone" });
+        this.state.tickets === 0 ? this.setState({ reqTickets: "dispBlock" }) : this.setState({ reqTickets: "dispNone" });
     }
 
     render() {
@@ -45,7 +82,6 @@ class BookShow extends Component {
                             <Typography variant="headline" component="h2">
                                 BOOK SHOW
                             </Typography><br />
-
                             <FormControl required className="formControl">
                                 <InputLabel htmlFor="location">Choose Location:</InputLabel>
                                 <Select
@@ -57,7 +93,78 @@ class BookShow extends Component {
                                         </MenuItem>
                                     ))}
                                 </Select>
+                                <FormHelperText className={this.state.reqLocation}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
                             </FormControl>
+                            <br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="language">Choose Language:</InputLabel>
+                                <Select
+                                    value={this.state.language}
+                                    onChange={this.languageChangeHandler}>
+                                    {language.map(lang => (
+                                        <MenuItem key={"lang" + lang.id} value={lang.language}>
+                                            {lang.language}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText className={this.state.reqLocation}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="showDate">Choose Show Date:</InputLabel>
+                                <Select
+                                    value={this.state.showDate}
+                                    onChange={this.showDateChangeHandler}>
+                                    {showDate.map(sd => (
+                                        <MenuItem key={"showDate" + sd.id} value={sd.showDate}>
+                                            {sd.showDate}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText className={this.state.reqLocation}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="showTime">Choose Show Time:</InputLabel>
+                                <Select
+                                    value={this.state.showTime}
+                                    onChange={this.showTimeChangeHandler}>
+                                    {showTime.map(st => (
+                                        <MenuItem key={"showTime" + st.id} value={st.showTime}>
+                                            {st.showTime}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText className={this.state.reqLocation}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br /><br />  
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="tickets">Tickets: ( {this.state.availableTickets} available )</InputLabel>
+                                <Input id="tickets" value={this.state.tickets !== 0 ? this.state.tickets : ""} onChange={this.ticketsChangeHandler} />
+                                <FormHelperText className={this.state.reqLocation}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br /><br />
+                            <Typography>
+                                Unit Price: Rs. {this.state.unitPrice}
+                            </Typography>
+                            <br />
+                            <Typography>
+                                Total Price: Rs. {this.state.unitPrice * this.state.tickets}
+                            </Typography>
+                            <br /><br />
+                            <Button variant="contained" onClick={this.bookShowButtonHandler} color="primary">
+                                BOOK SHOW
+                            </Button>  
                         </CardContent>
                     </Card>
                 </div>
